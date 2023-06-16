9b62497dd135cfc85dea9c164e99d1118a89ef45
chika-12 <markworship001@gmail.com>
2023-06-16 15:07:31 +0100

    C_variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints all base 10 numbers
+ * from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
