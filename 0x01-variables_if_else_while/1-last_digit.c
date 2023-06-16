0473acf97bfaf83680dd6e12c5c71b8905a56922
chika-12 <markworship001@gmail.com>
2023-06-16 13:30:23 +0100

    C_variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	}
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greter than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
 	if (last_digit == 0)
 	{
