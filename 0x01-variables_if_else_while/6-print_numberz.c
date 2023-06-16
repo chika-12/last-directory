94794d27c768ccf9d8876eff13b032fcd49a88a6
chika-12 <markworship001@gmail.com>
2023-06-16 15:12:57 +0100

    C_variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints numbers from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar(10);
+	return (0);
+}
