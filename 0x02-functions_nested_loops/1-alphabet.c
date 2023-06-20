aa4f1f83b38503fa0ca26a094c633fd8c848c378
chika-12 <markworship001@gmail.com>
2023-06-20 08:41:35 +0100

    C_function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- * prints lower case characters
+ * print_alphabet - Entry point
+ * lower case characters
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char alph;
-	
+
 	for (alph = 97; alph <= 122; alph++)
 	{
 		_putchar(alph);
