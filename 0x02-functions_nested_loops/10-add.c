a4298e2d41fa520c5ca1ecb49572a3d0e8d14706
chika-12 <markworship001@gmail.com>
2023-06-21 03:00:01 +0100

    C_function

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * add - Adds two numbers 
+ * add - Adds two numbers
  * @x: parameter
  * @y: parameter
  * Return: Always 0 (Success)
  */
 int add(int x, int y)
 {
-	return(x + y);
+	return (x + y);
 }
