05318e0a62b82ecc79ffb4a724d554317c18b486
chika-12 <markworship001@gmail.com>
2023-06-23 10:33:06 +0100

    C_Function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (num % 3 == 0 && num % 5 != 0)
+		else if (num % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (num % 3 != 0 && num % 5 == 0)
+		else if (num % 5 == 0)
 		{
 			printf("Buzz ");
 		}
