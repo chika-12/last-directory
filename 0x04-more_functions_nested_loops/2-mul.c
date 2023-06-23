42d7906438dcb14d902fafb9f781a429ae6b547f
chika-12 <markworship001@gmail.com>
2023-06-23 01:03:02 +0100

    C_Function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two input values
+ * @a: input variabe
+ * @b: input variable
+ * Return: 0;
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
