cb0cb97caa4a83dba51e6398477d3c02883fa929
chika-12 <markworship001@gmail.com>
2023-06-23 03:41:44 +0100

    C_Function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal line
+ * @n: parameter
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (y = 1; y <= x; y++)
+			{
+				_putchar(' ');
+			}
+				for (j = 1; j < 2; j++)
+				{
+					_putchar(92);
+				}
+
+			_putchar(10);
+		}
+	}
+}
