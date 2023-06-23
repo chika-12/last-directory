cba5bf46854def0a122d17fd20fea5cacf3c616e
chika-12 <markworship001@gmail.com>
2023-06-23 03:10:34 +0100

    C_Functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - prints line
+ * @n: variable for taking input
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int index;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (index = 1; index <= n; index++)
+		{
+			_putchar(45);
+		}
+		_putchar(10);
+	}
+}
