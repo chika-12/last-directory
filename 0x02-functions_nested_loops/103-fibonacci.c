adcefb3c610a880ccf7ec8afc82a5f1b087689f7
chika-12 <markworship001@gmail.com>
2023-06-25 05:32:29 +0100

    C_Function

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	long int fib;
 	long int running_sum = 0;
 
-	for (num = 1; fib <= 4000000; num++)
+	for (num = 0; fib <= 4000000; num++)
 	{
 		fib = num1 + num2;
 		if (fib % 2 == 0)
