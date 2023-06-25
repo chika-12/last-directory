068a86dcba9f24f5d0608a7c1f22363ac6617068
chika-12 <markworship001@gmail.com>
2023-06-25 02:57:48 +0100

    C_Function

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints multiples of 3 and 5 
+ * in 1024
+ *
+ * Return: Multiples of 3 and 5 in 1024
+ */
+int main(void)
+{
+	int rundown;
+	int multiple = 0;
+
+	for (rundown = 1024; rundown >= 1; rundown--)
+	{
+		if (rundown % 3 == 0 || rundown % 5 == 0)
+		{
+			multiple += rundown;
+		}
+	}
+	printf("%d\n", multiple);
+	return (0);
+}
