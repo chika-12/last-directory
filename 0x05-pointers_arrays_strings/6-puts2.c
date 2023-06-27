ab9c49763dfa9cab55219a6521cfad8c58eeba13
chika-12 <markworship001@gmail.com>
2023-06-27 03:34:34 +0100

    C_Pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * begining from the first character
+ *
+ * @str: parameter
+ * Return: string
+ */
+void puts2(char *str)
+{
+	int num;
+
+	for (num = 0; num <= str[num]; num += 2)
+	{
+		printf("%c", str[num]);
+	}
+	printf("\n");
+}
