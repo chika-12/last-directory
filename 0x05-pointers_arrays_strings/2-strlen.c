eaefd347cd36f5bb79cd73a8c0f73091a0914ae9
chika-12 <markworship001@gmail.com>
2023-06-27 02:19:18 +0100

    C_pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - prints the lenght of a string
+ * @s: parameter
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
