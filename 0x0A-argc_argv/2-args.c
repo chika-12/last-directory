9a428f3a94db0f0a702ec04ea3e73307c11902b5
chika-12 <markworship001@gmail.com>
2023-07-10 16:30:40 +0100

    C_Args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,12 +12,9 @@ int main(int argc __attribute__((unused)), char *argv[])
 {
 	int count;
 
-	if (argc > 1)
-	{
 		for (count = 0; count < argc; count++)
 		{
 			printf("%s\n", argv[count]);
 		}
-	}
 	return (0);
 }
