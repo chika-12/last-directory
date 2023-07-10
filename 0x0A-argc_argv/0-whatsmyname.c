a5e36d77e12d9bc80aa9173a934af49c8fbe8abc
chika-12 <markworship001@gmail.com>
2023-07-10 15:17:35 +0100

    C_Args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,9 +11,12 @@ int main(int argc, char *argv[])
 {
 	int count;
 
-	if (count >= 0)
+	if (argc >= 0)
 	{
-		printf("%s\n", argv[0]);
+		for (count = 0; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
 	}
 	return (0);
 }
