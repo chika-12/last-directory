1f469d71f376c505ac8df42658cd798052ede5ab
chika-12 <markworship001@gmail.com>
2023-07-10 15:11:26 +0100

    C_Args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints its name
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: Argc and Argv
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	if (count >= 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains programs expressing the useage of command line arguments
