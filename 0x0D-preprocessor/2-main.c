8d5741e43c5d81622c3a82ee1226cdb4812a21c2
chika-12 <markworship001@gmail.com>
2023-07-17 15:35:05 +0100

    C_Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define NAME "02-main.c"
 /**
  * main - defines the name of the program
  * Return: 0
  */
 int main(void)
 {
-	char *c;
-	c = NAME;
-	printf("%s\n", c);
+
+	printf("%s\n", __FILE__);
 	return (0);
 }
