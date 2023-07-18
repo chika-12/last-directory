cac786a06a1a093bc140ec11a3a3c28050f8606b
chika-12 <markworship001@gmail.com>
2023-07-18 15:37:14 +0100

    C_Structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,22 +9,14 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name == NULL)
-		{
+		if (!(d->name))
 			printf("Name: (nill)\n");
-		}
 		else
-		{
 			printf("Name: %s\n", d->name);
-		}
 		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-		{
+		if (!(d->owner))
 			printf("Owner: (nill)\n");
-		}
 		else
-		{
 			printf("Owner: %s\n", d->owner);
-		}
 	}
 }
