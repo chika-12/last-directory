a208f4ac08126b3cfd7f816b608719aa27749749
chika-12 <markworship001@gmail.com>
2023-07-19 15:01:37 +0100

    C_Function_pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - prints array
+ * @array: variable containing the array
+ * @size: array size
+ * @action: function pointers
+ * Return: Array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
