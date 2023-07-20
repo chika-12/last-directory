00e445ed8e82599e141fdc74fa772149da439638
chika-12 <markworship001@gmail.com>
2023-07-21 00:17:07 +0100

    C_Variadic_function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,6 +10,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
+	if (list == NULL)
+		printf("nil\n");
+
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
