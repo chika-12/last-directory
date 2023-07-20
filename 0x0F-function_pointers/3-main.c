320e376e1f809d54630dc3140df19a1da5a2e9ac
chika-12 <markworship001@gmail.com>
2023-07-20 01:09:31 +0100

    C_Function_pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - prints from commandline
+ * @argc: parameter
+ * @argc: parameter
+ * Return: operation
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	operation = get_op_func(argv[2]);
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", operation(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two intergers
+ * @a: parameter
+ * @b: parameter
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - prints the dividend of two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: dividen
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the modulus of two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a % b);
+}
