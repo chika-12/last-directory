7595eee2cde49b4111cc1761cc7fddedf89b596e
chika-12 <markworship001@gmai.com>
2023-08-01 16:41:10 +0100

    More linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_listint - prints the list of integers
+ * @h: pointer containing the first node
+ * Return: 0
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count;
+	const listint_t *ptr = h;
+
+	count = 0;
+	if (h == NULL)
+	{
+		printf("Empty list\n");
+	}
+	else
+	{
+		while (ptr != NULL)
+		{
+			count++;
+			printf("%d\n", ptr->n);
+			ptr = ptr->next;
+		}
+	}
+	return (count);
+}
+
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains c_source code for the implementation of more linked_list.
