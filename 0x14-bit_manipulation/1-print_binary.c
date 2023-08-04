8576932c2434317fa897c70ef1607e1e5f7d0c00
chika-12 <markworship001@gmai.com>
2023-08-04 04:05:02 +0100

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,48 @@
 #include "main.h"
+
 /**
- * print_binary - prints the binary representation of a number
- * @n: tenary number
- * Return: binary number
+ * _pow - calculates
+ * @base: parameter
+ * @power: power
+ *
+ * Return: value
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= power; i++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - prints a number in binary
+ * @n: number
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int i;
-	unsigned long int numbit;
-	unsigned long int help;
+	unsigned long int divisor, check;
+	char flag;
 
-	numbit = (sizeof(unsigned long int) * 8);
-	help = 1UL << (numbit - 1);
-
-	for (i = 0; i < numbit; i++)
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
 	{
-		if ((n & help) != 0)
+		check = n & divisor;
+		if (check == divisor)
 		{
+			flag = 1;
 			_putchar('1');
 		}
-		else
+		else if (flag == 1 || divisor == 1)
 		{
 			_putchar('0');
 		}
-		help >>= 1;
+		divisor >>= 1;
 	}
-	_putchar('\n');
 }
