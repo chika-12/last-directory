0811f5bde6e61796347c9990f95508587656e644
chika-12 <markworship001@gmai.com>
2023-08-12 00:34:44 +0100

    File_Handling

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,9 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * check_IO_state - checks file
  * @state: describes file to be opened
@@ -9,7 +14,7 @@
  */
 void check_IO_state(int state, int ptr, char *filename, char mode)
 {
-	if (mode == 'C' && stat == -1)
+	if (mode == 'C' && state == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close ptr %d\n", ptr);
 		exit(100);
@@ -45,23 +50,23 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	src = open(argv[1], O_RDONLY);
-	check_IO_stat(src, -1, argv[1], 'O');
+	check_IO_state(src, -1, argv[1], 'O');
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	check_IO_stat(dest, -1, argv[2], 'W');
+	check_IO_state(dest, -1, argv[2], 'W');
 	while (n_read == 1024)
 	{
 		n_read = read(src, buffer, sizeof(buffer));
 		if (n_read == -1)
-			check_IO_stat(-1, -1, argv[1], 'O');
+			check_IO_state(-1, -1, argv[1], 'O');
 		wrote = write(dest, buffer, n_read);
 		if (wrote == -1)
-			check_IO_stat(-1, -1, argv[2], 'W');
+			check_IO_state(-1, -1, argv[2], 'W');
 	}
 
 	close_src = close(src);
-	check_IO_stat(close_src, src, NULL, 'C');
+	check_IO_state(close_src, src, NULL, 'C');
 	close_dest = close(dest);
-	check_IO_stat(close_dest, dest, NULL, 'C');
+	check_IO_state(close_dest, dest, NULL, 'C');
 
 	return (0);
 }
