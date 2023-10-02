356389e71858d4de8afefa7b228bb272e656951d
chika-12 <markworship001@gmail.com>
2023-10-02 19:10:11 +0100

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,42 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
- * check_IO_state - checks file
- * @state: describes file to be opened
- * @filename: name of file
- * @mode: mode of operation
- * @ptr: descriptor
- * Return: 0
- */
-void check_IO_state(int state, int ptr, char *filename, char mode)
-{
-	if (mode == 'C' && state == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close ptr %d\n", ptr);
-		exit(100);
-	}
-	else if (mode == 'O' && state == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	else if (mode == 'W' && state == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-}
-/**
- * main - copies the content of file to file
+ * main - copies the content of one file to another
  * @argc: argument count
- * @argv: arguments velocity
- * Return: 1
+ * @argv: arguments passed
+ *
+ * Return: 1 on success, exit otherwise
  */
 int main(int argc, char *argv[])
 {
 	int src, dest, n_read = 1024, wrote, close_src, close_dest;
-
 	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
 	char buffer[1024];
 
 	if (argc != 3)
@@ -45,23 +25,49 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	src = open(argv[1], O_RDONLY);
-	check_IO_state(src, -1, argv[1], 'O');
+	check_IO_stat(src, -1, argv[1], 'O');
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	check_IO_state(dest, -1, argv[2], 'W');
+	check_IO_stat(dest, -1, argv[2], 'W');
 	while (n_read == 1024)
 	{
 		n_read = read(src, buffer, sizeof(buffer));
 		if (n_read == -1)
-			check_IO_state(-1, -1, argv[1], 'O');
+			check_IO_stat(-1, -1, argv[1], 'O');
 		wrote = write(dest, buffer, n_read);
 		if (wrote == -1)
-			check_IO_state(-1, -1, argv[2], 'W');
+			check_IO_stat(-1, -1, argv[2], 'W');
 	}
-
 	close_src = close(src);
-	check_IO_state(close_src, src, NULL, 'C');
+	check_IO_stat(close_src, src, NULL, 'C');
 	close_dest = close(dest);
-	check_IO_state(close_dest, dest, NULL, 'C');
-
+	check_IO_stat(close_dest, dest, NULL, 'C');
 	return (0);
+}
+
+/**
+ * check_IO_stat - checks if a file can be opened or closed
+ * @stat: file descriptor of the file to be opened
+ * @filename: name of the file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check_IO_stat(int stat, int fd, char *filename, char mode)
+{
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	else if (mode == 'W' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
